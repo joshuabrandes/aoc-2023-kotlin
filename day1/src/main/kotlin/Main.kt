4223a45498728cd154e2c58bf3895b80a3077e6b
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-01 17:25:18 +0100

    who hurt you

--- day1/src/main/kotlin/Main.kt
+++ day1/src/main/kotlin/Main.kt
@@ -1,23 +1,30 @@
 import java.io.File
 
+val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val startChars = listOf("o", "t", "f", "s", "e", "n")
 fun main(args: Array<String>) {
     println("------ Advent of Code 2023 - Day 1 -----")
     val result: Int = getPuzzleInput()
         .sumOf { getCalibrationValue(it) }
 
-    println("Result: $result")
+    println("Result Task 1: $result")
     println("----------------------------------------")
 }
 
-fun getCalibrationValue(text: String): Int {
+fun getCalibrationValue(text: String, includeWrittenNumber: Boolean): Int {
     val firstDigit = getFirstDigit(text)
     val lastDigit = getLastDigit(text)
 
     return firstDigit * 10 + lastDigit
 }
 
-fun getFirstDigit(text: String): Int {
+fun getFirstDigit(text: String, includeWrittenNumber: Boolean): Int {
     for (i in text.indices) {
+        if (startChars.contains(text[i].toString())) {
+            if (matchStartChar(text, i) != null) {
+                return matchStartChar(text, i)!!
+            }
+        } else
         if (text[i].isDigit()) {
             // println("Calibration value: ${text[i]}")
             return text[i].digitToInt()
@@ -26,14 +33,40 @@ fun getFirstDigit(text: String): Int {
     throw Exception("No digit found")
 }
 
-fun getLastDigit(text: String): Int {
+fun getLastDigit(text: String, includeWrittenNumber: Boolean): Int {
     for (i in text.length - 1 downTo 0) {
-        if (text[i].isDigit()) {
+        if (startChars.contains(text[i].toString())) {
+            if (matchStartChar(text, i) != null) {
+                return matchStartChar(text, i)!!
+            }
+        } else if (text[i].isDigit()) {
             // println("Calibration value: ${text[i]}")
             return text[i].digitToInt()
         }
     }
     throw Exception("No digit found")
+}
+
+fun containsNumber(text: String, i: Int, s: String): Boolean {
+    val substringMaxFive =
+        if (i + 5 > text.length) text.substring(i)
+        else text.substring(i, i + 5)
+    return substringMaxFive.contains(s)
+}
+
+fun matchStartChar(text: String, i: Int): Int? {
+    return when {
+        text[i] == 'o' && containsNumber(text, i, "one") -> 1
+        text[i] == 't' && containsNumber(text, i, "two") -> 2
+        text[i] == 't' && containsNumber(text, i, "three") -> 3
+        text[i] == 'f' && containsNumber(text, i, "four") -> 4
+        text[i] == 'f' && containsNumber(text, i, "five") -> 5
+        text[i] == 's' && containsNumber(text, i, "six") -> 6
+        text[i] == 's' && containsNumber(text, i, "seven") -> 7
+        text[i] == 'e' && containsNumber(text, i, "eight") -> 8
+        text[i] == 'n' && containsNumber(text, i, "nine") -> 9
+        else -> null
+    }
 }
 
 fun getPuzzleInput(): List<String> {

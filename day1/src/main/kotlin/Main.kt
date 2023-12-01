c0f2d6c47a910027d101d701a9ac744758a8d684
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-01 14:59:33 +0100

    finishing Day 1 –– well crap.

--- day1/src/main/kotlin/Main.kt
+++ day1/src/main/kotlin/Main.kt
@@ -1,7 +1,42 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("------ Advent of Code 2023 - Day 1 -----")
+    val result: Int = getPuzzleInput()
+        .sumOf { getCalibrationValue(it) }
+
+    println("Result: $result")
+    println("----------------------------------------")
+}
+
+fun getCalibrationValue(text: String): Int {
+    val firstDigit = getFirstDigit(text)
+    val lastDigit = getLastDigit(text)
+
+    return firstDigit * 10 + lastDigit
+}
+
+fun getFirstDigit(text: String): Int {
+    for (i in text.indices) {
+        if (text[i].isDigit()) {
+            // println("Calibration value: ${text[i]}")
+            return text[i].digitToInt()
+        }
+    }
+    throw Exception("No digit found")
+}
+
+fun getLastDigit(text: String): Int {
+    for (i in text.length - 1 downTo 0) {
+        if (text[i].isDigit()) {
+            // println("Calibration value: ${text[i]}")
+            return text[i].digitToInt()
+        }
+    }
+    throw Exception("No digit found")
+}
+
+fun getPuzzleInput(): List<String> {
+    val fileUrl = ClassLoader.getSystemResource("puzzle-input.txt")
+    return File(fileUrl.toURI()).readLines()
 }

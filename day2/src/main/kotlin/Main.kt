23f751b2fabfeba5ec76e6dd39ccf161ee8007e6
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-02 14:33:03 +0100

    minor refactorings and removing unnecessary code -- This is supposed to crash

--- day2/src/main/kotlin/Main.kt
+++ day2/src/main/kotlin/Main.kt
@@ -4,10 +4,6 @@ const val NUMBER_OF_RED = 12
 const val NUMBER_OF_GREEN = 13
 const val NUMBER_OF_BLUE = 14
 
-var minNumberOfRed = 0
-var minNumberOfGreen = 0
-var minNumberOfBlue = 0
-
 fun main() {
     println("------ Advent of Code 2023 - Day 2 -----")
 
@@ -30,6 +26,70 @@ fun main() {
 fun getPuzzleInput(): List<String> {
     val fileUrl = ClassLoader.getSystemResource("day2-input.txt")
     return File(fileUrl.toURI()).readLines()
+}
+
+data class Game(val gameId: Int, val turns: List<Turn>) {
+
+    fun isGamePossible(): Boolean {
+        for (turn in turns) {
+            if (!turn.isTurnPossible()) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun toString(): String {
+        return "Game $gameId: ${turns.joinToString("; ")}"
+    }
+
+    companion object {
+        fun fromString(input: String): Game {
+            val infos = input.split(": ")
+                .map { it.trim() }
+            assert(infos.size == 2) { "Invalid input: $input" }
+
+            // get gameId after "Game "
+            val gameId = infos[0].substring(5).toInt()
+            val turns = infos[1].split("; ")
+                .map { it.trim() }
+                .map { Turn.fromString(it) }
+
+            return Game(gameId, turns)
+        }
+    }
+}
+
+data class Turn(val turnResult: Map<Color, Int>) {
+
+    override fun toString(): String {
+        return turnResult.map { "${it.value} ${it.key.color}" }.joinToString(", ")
+    }
+
+    fun isTurnPossible(): Boolean {
+        val red = turnResult[Color.RED] ?: 0
+        val green = turnResult[Color.GREEN] ?: 0
+        val blue = turnResult[Color.BLUE] ?: 0
+
+        return red <= NUMBER_OF_RED
+                && green <= NUMBER_OF_GREEN
+                && blue <= NUMBER_OF_BLUE
+    }
+
+    companion object {
+        fun fromString(inputString: String): Turn {
+            val infoMap = inputString.split(", ")
+                .map { it.trim() }
+                .map {
+                    val parts = it.split(" ")
+                    assert(parts.size == 2) { "Invalid input: $inputString" }
+                    return@map parts
+                }
+                .associate { Color.fromString(it[1]) to it[0].toInt() }
+
+            return Turn(infoMap)
+        }
+    }
 }
 
 data class MinimumColorCount(val red: Int, val green: Int, val blue: Int) {
@@ -61,79 +121,6 @@ data class MinimumColorCount(val red: Int, val green: Int, val blue: Int) {
             }
 
             return MinimumColorCount(red, green, blue)
-        }
-    }
-}
-
-data class Game(private val _gameId: Int, private val _turns: List<Turn>) {
-
-    val gameId: Int
-        get() = _gameId
-
-    val turns: List<Turn>
-        get() = _turns
-
-    fun isGamePossible(): Boolean {
-        for (turn in _turns) {
-            if (!turn.isTurnPossible()) {
-                return false
-            }
-        }
-        return true
-    }
-
-    override fun toString(): String {
-        return "Game $_gameId: ${_turns.joinToString("; ")}"
-    }
-
-    companion object {
-        fun fromString(input: String): Game {
-            val infos = input.split(": ")
-                .map { it.trim() }
-            assert(infos.size == 2) { "Invalid input: $input" }
-
-            // get gameId after "Game "
-            val gameId = infos[0].substring(5).toInt()
-            val turns = infos[1].split("; ")
-                .map { it.trim() }
-                .map { Turn.fromString(it) }
-
-            return Game(gameId, turns)
-        }
-    }
-}
-
-data class Turn(private val _turnResult: Map<Color, Int>) {
-
-    val turnResult: Map<Color, Int>
-        get() = _turnResult
-
-    override fun toString(): String {
-        return _turnResult.map { "${it.value} ${it.key.color}" }.joinToString(", ")
-    }
-
-    fun isTurnPossible(): Boolean {
-        val red = _turnResult[Color.RED] ?: 0
-        val green = _turnResult[Color.GREEN] ?: 0
-        val blue = _turnResult[Color.BLUE] ?: 0
-
-        return red <= NUMBER_OF_RED
-                && green <= NUMBER_OF_GREEN
-                && blue <= NUMBER_OF_BLUE
-    }
-
-    companion object {
-        fun fromString(inputString: String): Turn {
-            val infoMap = inputString.split(", ")
-                .map { it.trim() }
-                .map {
-                    val parts = it.split(" ")
-                    assert(parts.size == 2) { "Invalid input: $inputString" }
-                    return@map parts
-                }
-                .associate { Color.fromString(it[1]) to it[0].toInt() }
-
-            return Turn(infoMap)
         }
     }
 }

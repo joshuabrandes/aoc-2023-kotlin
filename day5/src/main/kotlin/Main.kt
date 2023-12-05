b75279262069582ade725a18c5583d2d15a1cdec
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-05 22:01:38 +0100

    hopefully day 5 complete... -- what the hell happened here

--- day5/src/main/kotlin/Main.kt
+++ day5/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 import java.io.File
 
+var seedToSoilMap: AlmanachMap? = null
+var soilToFertilizerMap: AlmanachMap? = null
+var fertilizerToWaterMap: AlmanachMap? = null
+var waterToLightMap: AlmanachMap? = null
+var lightToTemperatureMap: AlmanachMap? = null
+var temperatureToHumidityMap: AlmanachMap? = null
+var humidityToLocationMap: AlmanachMap? = null
+
 fun main() {
     println("------ Advent of Code 2023 - Day  -----")
 
@@ -7,31 +15,54 @@ fun main() {
 
     val seeds = getSeeds(puzzleInput)
 
-    val locations = mapSeedsToLocation(seeds, puzzleInput)
+    seedToSoilMap = getMapByName("seed-to-soil map", puzzleInput)
+    soilToFertilizerMap = getMapByName("soil-to-fertilizer map", puzzleInput)
+    fertilizerToWaterMap = getMapByName("fertilizer-to-water map", puzzleInput)
+    waterToLightMap = getMapByName("water-to-light map", puzzleInput)
+    lightToTemperatureMap = getMapByName("light-to-temperature map", puzzleInput)
+    temperatureToHumidityMap = getMapByName("temperature-to-humidity map", puzzleInput)
+    humidityToLocationMap = getMapByName("humidity-to-location map", puzzleInput)
+
+    val locations = seeds.asSequence()
+        .map { seed -> mapSeedsToLocation(seed) }
 
     println("Task 1: Minimum location: ${locations.minOrNull()}")
+
+    // --- Task 2 ---
+
+    val minimumSeedLocation = findMinimumLocationForSeedRanges(seeds)
+
+    println("Task 2: Minimum location: $minimumSeedLocation")
 
     println("----------------------------------------")
 }
 
-fun mapSeedsToLocation(seeds: List<Long>, puzzleInput: List<String>): List<Long> {
-    val seedToSoilMap = getMapByName("seed-to-soil map", puzzleInput)
-    val soilToFertilizerMap = getMapByName("soil-to-fertilizer map", puzzleInput)
-    val fertilizerToWaterMap = getMapByName("fertilizer-to-water map", puzzleInput)
-    val waterToLightMap = getMapByName("water-to-light map", puzzleInput)
-    val lightToTemperatureMap = getMapByName("light-to-temperature map", puzzleInput)
-    val temperatureToHumidityMap = getMapByName("temperature-to-humidity map", puzzleInput)
-    val humidityToLocationMap = getMapByName("humidity-to-location map", puzzleInput)
+fun findMinimumLocationForSeedRanges(seeds: List<Long>): Long {
+    return seeds
+        .chunked(2)
+        .fold(Long.MAX_VALUE) { minLocation, seedRange ->
+            val (start, length) = seedRange
+            val end = start + length
 
-    return seeds.asSequence()
-        .map(seedToSoilMap::getByInput)
-        .map(soilToFertilizerMap::getByInput)
-        .map(fertilizerToWaterMap::getByInput)
-        .map(waterToLightMap::getByInput)
-        .map(lightToTemperatureMap::getByInput)
-        .map(temperatureToHumidityMap::getByInput)
-        .map(humidityToLocationMap::getByInput)
-        .toList()
+            (start..<end).fold(minLocation) { currentMin, seed ->
+                minOf(currentMin, mapSeedsToLocation(seed))
+            }
+        }
+}
+
+fun mapSeedsToLocation(seed: Long): Long {
+
+    return listOfNotNull(
+        seedToSoilMap,
+        soilToFertilizerMap,
+        fertilizerToWaterMap,
+        waterToLightMap,
+        lightToTemperatureMap,
+        temperatureToHumidityMap,
+        humidityToLocationMap
+    ).fold(seed) { input, map ->
+        map.getByInput(input)
+    }
 }
 
 fun getMapByName(delimiter: String, puzzleInput: List<String>): AlmanachMap {
@@ -40,7 +71,8 @@ fun getMapByName(delimiter: String, puzzleInput: List<String>): AlmanachMap {
             val map = mutableListOf<List<Long>>()
             var lineDela = lineNumber + 1
 
-            while (puzzleInput[lineDela].isNotEmpty() && lineDela < puzzleInput.size) {
+            // Stelle sicher, dass lineDela innerhalb der Grenzen von puzzleInput bleibt
+            while (lineDela < puzzleInput.size && puzzleInput[lineDela].isNotEmpty()) {
                 val line = puzzleInput[lineDela]
                 val row = line.split(" ")
                     .map { it.trim() }
@@ -56,6 +88,7 @@ fun getMapByName(delimiter: String, puzzleInput: List<String>): AlmanachMap {
     }
     throw Exception("Map not found")
 }
+
 
 fun getSeeds(puzzleInput: List<String>): List<Long> {
     return puzzleInput[0]
@@ -78,7 +111,8 @@ data class AlmanachMap(val map: List<List<Long>>) {
                 return row[0] + (input - row[1])
             }
         }
-        throw Exception("Input not found")
+        // if no row was found, return input
+        return input
     }
 
     private fun valueIsInRange(input: Long, row: List<Long>): Boolean {

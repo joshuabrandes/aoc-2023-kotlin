c2b95babc9207612f983c59a1b54204f18a47ed9
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-06 07:53:42 +0100

    You can't see it, but I'm making a very angry face right now

--- day6/src/main/kotlin/Main.kt
+++ day6/src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
+import java.io.File
+
 fun main() {
     println("------ Advent of Code 2023 - Day  -----")
 
     println("----------------------------------------")
 }
+
+fun getPuzzleInput(): List<String> {
+    val fileUrl = ClassLoader.getSystemResource("input.txt")
+    return File(fileUrl.toURI()).readLines()
+}
+

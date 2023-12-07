6a51bff740e94ac8ccaabffcc46027ee34b14b01
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-07 16:27:01 +0100

    adding day 7 –– did everything

---
+++ day7/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package org.example
+
+import java.io.File
+
+fun main() {
+    println("------ Advent of Code 2023 - Day 6 -----")
+
+    val puzzleInput = getPuzzleInput()
+
+    println("----------------------------------------")
+}
+
+fun getPuzzleInput(): List<String> {
+    val fileUrl = ClassLoader.getSystemResource("input.txt")
+    return File(fileUrl.toURI()).readLines()
+}

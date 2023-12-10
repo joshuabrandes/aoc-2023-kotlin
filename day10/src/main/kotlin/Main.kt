2868029c364ac04a1042629f4930da8acdc0e0ba
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2023-12-10 14:41:00 +0100

    day9 input -- I don't know what the hell I was thinking.

--- day10/src/main/kotlin/Main.kt
+++ day10/src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.example
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    println("------ Advent of Code 2023 - Day 10 -----")
+
+    val puzzleInput = getPuzzleInput()
+
+
+
+    println("----------------------------------------")
+}
+
+fun getPuzzleInput(): List<String> {
+    val fileUrl = ClassLoader.getSystemResource("input.txt")
+    return File(fileUrl.toURI()).readLines()
 }
